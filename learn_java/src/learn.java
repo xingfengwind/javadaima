9f54b3e7ed845a2adbba539ffca834536fe2bf8e
windnew <fr060116@outlook.com>
2025-03-01 14:42:00 +0800

    付款

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -4,10 +4,10 @@ public  class learn {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
        int money=sc.nextInt();
-       if(money>=100)
-           System.out.println("网红餐厅");
+       if(money>=600)
+           System.out.println("付款成功");
        else
-           System.out.println("沙县小吃");
+           System.out.println("付款失败");
 
 
 

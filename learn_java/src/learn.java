88c643c6d94a96e8ea002075897e554a3fbbc8c6
windnew <fr060116@outlook.com>
2025-03-01 14:33:40 +0800

    红绿灯

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public  class learn {
     public static void main(String[] args) {
-        int height1=150;
-        int height2=210;
-        int height3=165;
-        int temp=height1>height2?height1:height2;
-        int max=temp>height3?temp:height3;
-        System.out.println(max);
+        Scanner sc=new Scanner(System.in);
+        boolean isLightGreen=sc.nextBoolean();
+        if(isLightGreen){
+            System.out.println("gogogo");
+        }
 
-
-
-
+        else{
+            System.out.println("Stop");
+        }
     }
 }

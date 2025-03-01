5136906d2e4f1f0d62a015b8ec7769395eae43ed
windnew <fr060116@outlook.com>
2025-03-01 14:52:18 +0800

    奖励

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public  class learn {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
-       int sign =sc.nextInt();
-       if(sign%2==0)
-           System.out.println("坐右边");
-       else
-           System.out.println("坐左边");
+       int grade=sc.nextInt();
+       if(grade>=95){
+           System.out.println("自行车");
+       }
+       else if(grade>=90){
+           System.out.println("游乐园");
+       }
+       else if(grade>=80){
+           System.out.println("变形金刚");
+       }
+       else{
+           System.out.println("挨顿揍");
+       }
 
 
 

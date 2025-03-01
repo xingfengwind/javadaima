b7690657cb3e820c21c0ef386bb6c7c59b4e75a0
windnew <fr060116@outlook.com>
2025-03-01 10:24:11 +0800

    隐式转换

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -2,15 +2,33 @@ import java.util.Scanner;
 
 public  class learn {
     public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
-      System.out.println("请输入一个三位数：");
-      int number=sc.nextInt();
-      int ones=number%10;
-      int tens=number/10%10;
-      int hundreds=number/100%10;
-      System.out.println(ones);
-      System.out.println(tens);
-      System.out.println(hundreds);
+     double d=10;
+     System.out.println(d);//10.0
 
+        byte b=100;
+        int i=b;
+        System.out.println(i);//100
+
+
+        int i1 = 10;
+        long n = 20L;
+        long result = i1 + n;
+        System.out.println(result); // 30
+
+        int a = 10;
+        long c = 100L;
+        double f = 20.0;
+        double result1 = a + c + f;
+        System.out.println(result1); // 130.0
+        byte b1 = 10;
+        byte b2 = 20;
+        int result2 = b1 + b2;//int
+        System.out.println(result2); // 30
+
+        byte b3 = 10;
+        short s = 20;
+        long n2 = 100L;
+        long result3 = b3 + s + n2;
+        System.out.println(result3); // 130
     }
 }

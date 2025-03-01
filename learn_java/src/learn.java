a61e26ba1d37f6fc230b4bfa2aabc4228f03ef36
windnew <fr060116@outlook.com>
2025-03-01 14:40:22 +0800

    餐厅选择

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 
 public  class learn {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        boolean isLightGreen=sc.nextBoolean();
-        boolean isLightRed=sc.nextBoolean();
-        boolean isLightYellow=sc.nextBoolean();
-        if(isLightGreen){
-            System.out.println("gogogo");
-        }
-        else if(isLightRed){
-            System.out.println("stop");
-        }
-        else if(isLightYellow){
-            System.out.println("slow down");
-        }
+       Scanner sc=new Scanner(System.in);
+       int money=sc.nextInt();
+       if(money>=100)
+           System.out.println("网红餐厅");
+       else
+           System.out.println("沙县小吃");
+
+
 
     }
 }

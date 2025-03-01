c60b3aec699da2e41b1813a46b8a2fbeae953b2d
windnew <fr060116@outlook.com>
2025-03-01 11:10:07 +0800

    三个值的最大值

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public  class learn {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("请输入第一老虎的体重：");
-        int weight1=sc.nextInt();
-        System.out.println("请输入第二只老虎的体重：");
-        int weight2=sc.nextInt();
-        String result=weight1==weight2?"相同":"不相同";
-        System.out.println(result);
+        int height1=150;
+        int height2=210;
+        int height3=165;
+        int temp=height1>height2?height1:height2;
+        int max=temp>height3?temp:height3;
+        System.out.println(max);
+
 
 
 

8d965013ebf9c76d01d2678cc305856a0def7398
windnew <fr060116@outlook.com>
2025-03-01 10:07:04 +0800

    运算符1加减乘除取余

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -1,5 +1,22 @@
-public class learn {
-    public static void main(String[] args){
-        System.out.println("Hello World!!");
+public  class learn {
+    public static void main(String[] args) {
+        //+
+        System.out.println(3+2);//5
+        //-
+        System.out.println(3-2);//1
+        //*
+        System.out.println(3*2);//6
+
+        //如果小数参加计算
+        //结论：小数参与计算，结果可能不精确
+        System.out.println(1.1+1.1);//2.2
+        System.out.println(1.1+1.01);//2.11
+        System.out.println(1.1*1.01);
+
+        System.out.println(10/3);//3
+        System.out.println(10.0/3);//3.3333333333333335
+        System.out.println(10%3);//1
+        System.out.println(10.0%3);//1.0
+
     }
 }

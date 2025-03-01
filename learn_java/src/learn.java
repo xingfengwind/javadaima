38d82eebc8d1388b826c8e0d4d09d4c0a9d97d0c
windnew <fr060116@outlook.com>
2025-03-01 10:13:17 +0800

    案例1

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -1,22 +1,16 @@
+import java.util.Scanner;
+
 public  class learn {
     public static void main(String[] args) {
-        //+
-        System.out.println(3+2);//5
-        //-
-        System.out.println(3-2);//1
-        //*
-        System.out.println(3*2);//6
-
-        //如果小数参加计算
-        //结论：小数参与计算，结果可能不精确
-        System.out.println(1.1+1.1);//2.2
-        System.out.println(1.1+1.01);//2.11
-        System.out.println(1.1*1.01);
-
-        System.out.println(10/3);//3
-        System.out.println(10.0/3);//3.3333333333333335
-        System.out.println(10%3);//1
-        System.out.println(10.0%3);//1.0
+      Scanner sc=new Scanner(System.in);
+      System.out.println("请输入一个三位数：");
+      int number=sc.nextInt();
+      int ones=number%10;
+      int tens=number/10%10;
+      int hundreds=number/100%10;
+      System.out.println(ones);
+      System.out.println(tens);
+      System.out.println(hundreds);
 
     }
 }

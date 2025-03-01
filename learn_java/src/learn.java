c483e5bc6e89af7b52354c3a30edcdb40bac5b49
windnew <fr060116@outlook.com>
2025-03-01 10:31:00 +0800

    字符串相加

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -2,33 +2,12 @@ import java.util.Scanner;
 
 public  class learn {
     public static void main(String[] args) {
-     double d=10;
-     System.out.println(d);//10.0
-
-        byte b=100;
-        int i=b;
-        System.out.println(i);//100
+        System.out.println(1+"abc"+1);
+        System.out.println(1+2+"abc"+2+1);
+        String name="hahah";
+        System.out.println("我的名字是"+name);
 
 
-        int i1 = 10;
-        long n = 20L;
-        long result = i1 + n;
-        System.out.println(result); // 30
 
-        int a = 10;
-        long c = 100L;
-        double f = 20.0;
-        double result1 = a + c + f;
-        System.out.println(result1); // 130.0
-        byte b1 = 10;
-        byte b2 = 20;
-        int result2 = b1 + b2;//int
-        System.out.println(result2); // 30
-
-        byte b3 = 10;
-        short s = 20;
-        long n2 = 100L;
-        long result3 = b3 + s + n2;
-        System.out.println(result3); // 130
     }
 }

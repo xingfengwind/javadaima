53103525f9a743f1676b1780fc4a80a15dc82205
windnew <fr060116@outlook.com>
2025-03-01 11:01:49 +0800

    两只老虎

--- learn_java/src/learn.java
+++ learn_java/src/learn.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public  class learn {
     public static void main(String[] args) {
-        char c = 'a';
-        int result = c + 0;
-        System.out.println(result);//97
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入第一老虎的体重：");
+        int weight1=sc.nextInt();
+        System.out.println("请输入第二只老虎的体重：");
+        int weight2=sc.nextInt();
+        String result=weight1==weight2?"相同":"不相同";
+        System.out.println(result);
+
 
 
     }

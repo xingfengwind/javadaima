38294b7bd8b89f33ea7ab7e8d29a673bb503e7b5
windnew <fr060116@outlook.com>
2025-03-16 20:23:38 +0800

    按从大到小顺序输出数字

--- learn_java/untitled1/src/learn.java
+++ learn_java/untitled1/src/learn.java
@@ -4,18 +4,36 @@ public class learn {
 
 
     public static void main(String[] args) {
+                Scanner sc=new Scanner(System.in);
+                int a=sc.nextInt();
+                int b=sc.nextInt();
+                int c=sc.nextInt();
+                print(a,b,c);
 
-        Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        int max_numb=max(a,b,c);
-        System.out.println(max_numb);
+
     }
 
+    public static void print(int a,int b,int c) {
 
-        public static int max(int a,int b,int c){
-            return (a>b?a:b)>c?(a>b?a:b):c;
+        int temp = 0;
+        if (a < b) {
+            temp = a;
+            a = b;
+            b = temp;
         }
+        if (a < c) {
+            temp = a;
+            a = c;
+            c = temp;
+        }
+        if (b < c) {
+            temp = b;
+            b = c;
+            c = temp;
+        }
+        System.out.println(a + " " + b + " " + c);
+
+    }
+
 
 }

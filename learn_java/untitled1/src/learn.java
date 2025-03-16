efb8925965c7d7643ddc23fe87bd86963c6c805f
windnew <fr060116@outlook.com>
2025-03-16 20:34:20 +0800

    绝对值

--- learn_java/untitled1/src/learn.java
+++ learn_java/untitled1/src/learn.java
@@ -1,39 +1,20 @@
 import java.util.Scanner;
 
-public class learn {
+public class learn{
 
-
-    public static void main(String[] args) {
-                Scanner sc=new Scanner(System.in);
-                int a=sc.nextInt();
-                int b=sc.nextInt();
-                int c=sc.nextInt();
-                print(a,b,c);
+        public static double acs (double a){
+            if(a<0){
+                return -a;
+            }
+            else{
+                return a;
+            }
+        }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        double a=sc.nextDouble();
+        System.out.println(acs(a));
 
 
     }
-
-    public static void print(int a,int b,int c) {
-
-        int temp = 0;
-        if (a < b) {
-            temp = a;
-            a = b;
-            b = temp;
-        }
-        if (a < c) {
-            temp = a;
-            a = c;
-            c = temp;
-        }
-        if (b < c) {
-            temp = b;
-            b = c;
-            c = temp;
-        }
-        System.out.println(a + " " + b + " " + c);
-
-    }
-
-
 }
